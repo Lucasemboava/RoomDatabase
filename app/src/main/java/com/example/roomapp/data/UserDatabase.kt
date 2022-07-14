73013237c00601f0aee6e803d78bcc4cc6af5ec6
Lucas Emboava <lucas.oliveira@usemobile.xyz>
2022-07-14 16:50:50 -0300

    Adding comment if it was an manual migration

--- app/src/main/java/com/example/roomapp/data/UserDatabase.kt
+++ app/src/main/java/com/example/roomapp/data/UserDatabase.kt
@@ -3,6 +3,8 @@ package com.example.roomapp.data
 import android.content.Context
 import androidx.room.*
 import androidx.room.migration.AutoMigrationSpec
+import androidx.room.migration.Migration
+import androidx.sqlite.db.SupportSQLiteDatabase
 import com.example.roomapp.model.User
 
 @Database(entities = [User::class], version = 3,
@@ -47,5 +49,15 @@ abstract class UserDatabase : RoomDatabase() {
         toColumnName  = "estado"
     )
     class RenameFromUfToEstado: AutoMigrationSpec{}
+
+//    if it was a manual migration
+//    val MIGRATION_2_3: Migration = Migration(2,3){
+//        fun migrate(db: SupportSQLiteDatabase){
+//            db.execSQL("CREATE TABLE IF NOT EXISTS _new_user_table ('id' INTEGER NOT NULL, firstName TEXT, lastName TEXT, PRIMARY KEY('id'))")
+//            db.execSQL("INSERT INTO '_new_user_table' (id, estado) SELECT id, uf FROM 'user_table'")
+//            db.execSQL("DROP TABLE 'user_table'")
+//            db.execSQL("ALTER TABLE '_new_user_table' RENAME TO 'user_table'")
+//        }
+//    }
 
 }
