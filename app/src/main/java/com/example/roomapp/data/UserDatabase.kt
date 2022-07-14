779ad16b855a5ba9433837d3710caa3b41aabd5b
Lucas Emboava <lucas.oliveira@usemobile.xyz>
2022-07-14 14:16:35 -0300

    PreAutoMigration
    Auto Migration done.

--- app/src/main/java/com/example/roomapp/data/UserDatabase.kt
+++ app/src/main/java/com/example/roomapp/data/UserDatabase.kt
@@ -7,10 +7,10 @@ import androidx.room.Room
 import androidx.room.RoomDatabase
 import com.example.roomapp.model.User
 
-@Database(entities = [User::class], version = 1,
+@Database(entities = [User::class], version = 2,
 
-    //version =2,
-    //autoMigrations = [AutoMigration (from = 1, to = 2)],
+
+    autoMigrations = [AutoMigration (from = 1, to = 2)],
 
     exportSchema = true)
 abstract class UserDatabase : RoomDatabase() {
--- app/src/main/java/com/example/roomapp/model/User.kt
+++ app/src/main/java/com/example/roomapp/model/User.kt
@@ -12,6 +12,6 @@ data class User(
     val id: Int,
     val firstName: String,
     val lastName: String,
-    val age: Int
-    //,val uf: String? = null
+    val age: Int,
+    val uf: String? = null
 ): Parcelable
